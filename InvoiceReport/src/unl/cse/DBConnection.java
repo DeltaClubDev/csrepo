69161e094931e04bb6efa97dd5f75a4b2a98a079
DeltaClubDev <jacobcharles300@gmail.com>
2014-04-16 22:33:01 -0700

    Update DBConnection.java

--- InvoiceReport/src/unl/cse/DBConnection.java
+++ InvoiceReport/src/unl/cse/DBConnection.java
@@ -6,7 +6,7 @@ package unl.cse;
  * @version 0.5.0
  */
 public class DBConnection {
-	public static final String DB_URL = "jdbc:mysql://cse.unl.edu/jcharles";
-	public static final String DB_USERNAME = "jcharles";
-	public static final String DB_PASSWORD = "VUM#=p";
+	public static final String DB_URL = "****";
+	public static final String DB_USERNAME = "****";
+	public static final String DB_PASSWORD = "****";
 }
